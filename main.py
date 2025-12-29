8d13e9082d290b621825a9965dee0b86468a84d0
aymene69 <aymene69@github.com>
2025-12-29 13:36:24 +0100

    Added search for Stremio

--- main.py
+++ main.py
@@ -664,3 +664,8 @@ async def get_meta(
     }
 
     return JSONResponse(content={"meta": meta})
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 3000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
